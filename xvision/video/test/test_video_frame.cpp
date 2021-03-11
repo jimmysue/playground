3818f0eccbdc47ff36d84af8b90e1d4a49d8c722
jimmy <su.jz@outlook.com>
2021-03-11 15:37:38 +0800

    add test cases

--- xvision/video/test/test_video_frame.cpp
+++ xvision/video/test/test_video_frame.cpp
@@ -65,8 +65,13 @@ void testVideoFrame() {
     VideoFrame f2(100, 332, xvision::VideoFrame::PixelFormat::AV_PIX_FMT_ARGB);
     f2 = std::move(f2);
     VideoFrame f3 = f2;
-    frame = f3;
-    std::swap(frame, f3);
+    {
+        frame = f3;
+        std::swap(frame, f3);
+        auto f4 = VideoFrame(100, 332,
+                             xvision::VideoFrame::PixelFormat::AV_PIX_FMT_ARGB);
+    }
+
     cout << "width: " << f2->width << ", "
          << "height: " << f2->height << ", : " << f2->format << endl;
     const AVFrame *ptr = f3;
